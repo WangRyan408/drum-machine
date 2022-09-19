9555d01e3c733f5e0cb6221244c5333c50857eac
Ryan Wang <rwryanwang@gmail.com>
2022-09-19 00:53:24 -0700

    changed file extensions, added code

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="drum-machine">
+      <div>
+        <div className="drum-pad" id="heater-1"></div>
+        <div className="drum-pad" id="heater-2"></div>
+        <div className="drum-pad" id="heater-3"></div>
+        <div className="drum-pad" id="heater-4"></div>
+        <div className="drum-pad" id="clap"></div>
+        <div className="drum-pad" id="open-hh"></div>
+        <div className="drum-pad" id="kick-n-hat"></div>
+        <div className="drum-pad" id="kick"></div>
+        <div className="drum-pad" id="closed-hh"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
