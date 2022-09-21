131a6b6bd517fd8234e6ce4a08a8dfcde3a1fd06
Ryan Wang <rwryanwang@gmail.com>
2022-09-20 20:04:40 -0700

    Added sound files

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ function App() {
   return (
     <div className="App" id="drum-machine">
       <div>
-        <div className="drum-pad" id="heater-1"></div>
-        <div className="drum-pad" id="heater-2"></div>
-        <div className="drum-pad" id="heater-3"></div>
-        <div className="drum-pad" id="heater-4"></div>
-        <div className="drum-pad" id="clap"></div>
-        <div className="drum-pad" id="open-hh"></div>
-        <div className="drum-pad" id="kick-n-hat"></div>
-        <div className="drum-pad" id="kick"></div>
-        <div className="drum-pad" id="closed-hh"></div>
+        <div className="drum-pad" id="heater-1" data-key=""><kbd>Q</kbd></div>
+        <div className="drum-pad" id="heater-2" data-key=""><kbd>W</kbd></div>
+        <div className="drum-pad" id="heater-3" data-key=""><kbd>E</kbd></div>
+        <div className="drum-pad" id="heater-4" data-key=""><kbd>A</kbd></div>
+        <div className="drum-pad" id="clap" data-key=""><kbd>S</kbd></div>
+        <div className="drum-pad" id="open-hh" data-key=""><kbd>D</kbd></div>
+        <div className="drum-pad" id="kick-n-hat"><kbd>Z</kbd></div>
+        <div className="drum-pad" id="kick"><kbd>X</kbd></div>
+        <div className="drum-pad" id="closed-hh"><kbd>C</kbd></div>
+        <audio src="../assets/Heater-1.mp3"></audio>
+        <audio src="../assets/Heater-2.mp3"></audio>
+        <audio src="../assets/Heater-3.mp3"></audio>
+        <audio src="../assets/Heater-4_1.mp3"></audio>
+        <audio src="../assets/Heater-6.mp3"></audio>
+        <audio src="../assets/Dsc_Oh.mp3"></audio>
+        <audio src="../assets/Cev_H2"></audio>
+        <audio src="./assets/Kick_n_Hat.mp3"></audio>
+        <audio src="../assets/RP4_KICK_1.mp3"></audio>
       </div>
     </div>
   );
